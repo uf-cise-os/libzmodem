10b68df98d76754550efd96106ef5a95b0098843
Martin A. Godisch <godisch@debian.org>
2018-05-01 15:34:22 +0200

    0.12.21-10 source

--- src/zm.c
+++ src/zm.c
@@ -432,10 +432,12 @@ zsdata(const char *buf, size_t length, int frameend)
 	VPRINTF(3,("zsdata: %lu %s", (unsigned long) length, 
 		Zendnames[(frameend-ZCRCE)&3]));
 	crc = 0;
-	do {
-		zsendline(*buf); crc = updcrc((0377 & *buf), crc);
-		buf++;
-	} while (--length>0);
+
+	for( ; length; length--) {
+	  zsendline(*buf); crc = updcrc((0377 & *buf), crc);
+	  buf++;
+	}
+
 	xsendline(ZDLE); xsendline(frameend);
 	crc = updcrc(frameend, crc);
 
